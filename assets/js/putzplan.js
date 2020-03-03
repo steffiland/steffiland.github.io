ebc86aefade9a5b8e3897cf84eef9d19c44e9513
steffiland <git01@steffiland.de>
2020-03-03 21:10:04 +0100

    Update putzplan.js

--- assets/js/putzplan.js
+++ assets/js/putzplan.js
@@ -1,9 +1,25 @@
-var $ppid = $("#pp-id").text();
+$( document ).ready(function() {
+  var $ppid = $("#pp-id").text();
 
-$( "li.task-list-item" ).click(function() {
-  var $itemId = $(this).text().replace(/[^A-Z0-9]/ig, "").toLowerCase();
-  console.log( $ppid + $itemId );
-  $( this ).children('input').each(function(){
-     $(this).prop('checked', !$(this)[0].checked);
-  });                         
+  $( "li.task-list-item" ).click(function() {
+    var $itemId = $(this).text().replace(/[^A-Z0-9]/ig, "").toLowerCase();
+    var $value;
+    $( this ).children('input').each(function(){
+      $(this).prop('checked', !$(this)[0].checked);
+      $value = $(this)[0].checked);
+    });
+    console.log( $ppid + $itemId + " := " + $value);
+    localStorage.setItem($ppid + $itemId , $value);
+  });
+
+  $( "li.task-list-item" ).each(function() {
+    var $itemId = $(this).text().replace(/[^A-Z0-9]/ig, "").toLowerCase();
+    var $value = localStorage.getItem($ppid + $itemId);
+    if ( $value != null ) {
+      $( this ).children('input').each(function(){
+        $(this).prop('checked', $value);
+      });
+      console.log( $ppid + $itemId + " = " + $value);
+    }
+  });
 });
