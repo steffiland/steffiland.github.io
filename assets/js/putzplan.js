6434d59927ccb1e1e4083d9bc8a5391c6dc95aeb
steffiland <git01@steffiland.de>
2020-03-03 16:23:23 +0100

    Update putzplan.js

--- assets/js/putzplan.js
+++ assets/js/putzplan.js
@@ -1,7 +1,7 @@
 $( "li.task-list-item" ).click(function() {
   console.log( $(this).text() );
   $( this ).children('input').each(function () {
-    this.prop("checked", !this.prop("checked"));
+    this.prop(":checked") = !this.prop(":checked");
   });
                               
 });
