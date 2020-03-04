504f538767bf53bdd09d4f8bf46f00583bb63fa2
steffiland <git01@steffiland.de>
2020-03-04 08:56:09 +0100

    Update putzplan.js

--- assets/js/putzplan.js
+++ assets/js/putzplan.js
@@ -1,3 +1,24 @@
+function weekOfMonth(wday, mday) {
+    var d = new Date();
+    if ( !wday ) { wday  = d.getDay(); }
+    //wday = 0 [Sun] ... 6 [Sat] (day of week)
+    if (!mday)  { mday= d.getDate(); }
+    //mday = 1 ... 31 (day of month)
+
+    //we start counting weeks with 1:
+    var wnum = 1;
+    var add = 2;
+    // if the month starts on a monday: ...
+    if (((mday - wday) %7) == 0 ) { add = 1; }
+
+    // if (mday < wday) { wnum = 1; }
+    if (mday >= wday) {
+        wnum = Math.floor((mday -wday)/7) + add;
+    }
+    return wnum; 
+
+}
+
 $( document ).ready(function() {
   var $ppid = $("#pp-id").text();
 
