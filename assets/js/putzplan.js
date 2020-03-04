722a3feaf649c3ccc0c2d0c011d37f204fcd29b3
steffiland <git01@steffiland.de>
2020-03-04 07:13:34 +0100

    Update putzplan.js

--- assets/js/putzplan.js
+++ assets/js/putzplan.js
@@ -8,18 +8,18 @@ $( document ).ready(function() {
       $(this).prop('checked', !$(this)[0].checked);
       $value = $(this)[0].checked;
     });
-    console.log( $ppid + $itemId + " := " + $value);
-    localStorage.setItem($ppid + $itemId , $value);
+    console.log( $ppid + '.' + $itemId + " := " + $value);
+    localStorage.setItem($ppid + '.' + $itemId , $value);
   });
 
   $( "li.task-list-item" ).each(function() {
     var $itemId = $(this).text().replace(/[^A-Z0-9]/ig, "").toLowerCase();
-    var $value = localStorage.getItem($ppid + $itemId);
+    var $value = localStorage.getItem($ppid + '.' + $itemId);
     if ( $value != null ) {
       $( this ).children('input').each(function(){
-        $(this).prop('checked', $value);
+        $(this).prop('checked', $value ? true : false);
       });
-      console.log( $ppid + $itemId + " = " + $value);
+      console.log( $ppid + '.' + $itemId + " = " + $value);
     }
   });
 });
